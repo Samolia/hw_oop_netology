944a37d24792dabcb331133ccc032752bdadc00b
samolia <samolia9@gmail.com>
2021-02-23 00:22:35 +0300

    Overload __str__ method and added scoring methods

--- university.py
+++ university.py
@@ -25,7 +25,36 @@ class Student:
         else:
             return 'Ошибка'
 
-        
+    def average_rate(self, course):
+        '''
+        Считает среднюю оценку студента за курс
+        '''
+        if course in self.courses_in_progress and len(self.grades[course]) > 0:
+            average_rate = (sum(self.grades[course]) / len(self.grades[course]))
+            return f'Средняя оценка студента "{self.name} {self.surname}" за курс "{course}": {round(average_rate, 2)}'
+        else:
+            return 'Ошибка'
+
+    def average_rate_hw(self):
+        '''
+        Считает среднюю оценку студента за все курсы
+        '''
+        all_rate = []
+        for course in self.grades:
+            all_rate.append(sum(self.grades[course]) / len(self.grades[course]))
+            general_ball = sum(all_rate) / len(all_rate)
+        return f'Средняя оценка студента "{self.name} {self.surname}" за все курсы - {round(general_ball, 2)}'
+
+    def __str__(self):
+        '''
+        Информация об объекте
+        print(some_student)
+        '''
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {(self.average_rate_hw()).split(" ")[-1]}\n' \
+               f'Курсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}'
+
+
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -37,6 +66,35 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         self.grades = {}
         super().__init__(name, surname)
+
+    def average_rate(self, course):
+        '''
+        Считает среднюю оценку лектора за курс
+        '''
+        if course in self.courses_attached and len(self.grades[course]) > 0:
+            average_rate = (sum(self.grades[course]) / len(self.grades[course]))
+            return f'Средняя оценка лектора "{self.name} {self.surname}" за курс "{course}": {round(average_rate, 2)}'
+        else:
+            return 'Ошибка'
+
+    def average_rate_all(self):
+        '''
+        Считает среднюю оценку лектора за все курсы
+        '''
+        all_rate = []
+        for course in self.grades:
+            all_rate.append(sum(self.grades[course]) / len(self.grades[course]))
+            general_ball = sum(all_rate) / len(all_rate)
+        return f'Средняя оценка лектора "{self.name} {self.surname}" за все курсы - {round(general_ball, 2)}'
+
+    def __str__(self):
+        '''
+        Информация об объекте
+        print(some_lecturer)
+        '''
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {(self.average_rate_all().split(" ")[-1])}'
+
+
 
 
 class Reviewer(Mentor):
@@ -54,3 +112,10 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        '''
+        Информация об объекте
+        print(some_reviewer)
+        '''
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
